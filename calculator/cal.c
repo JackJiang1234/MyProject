de854a41e431133b548bc9ed54ce502130183c87
jjy_8888@163.com <jjy_8888@163.com>
2016-05-13 11:04:40 -0400

    add cal c file

---
+++ calculator/cal.c
@@ -0,0 +1,26 @@
+/*************************************************************************
+	> File Name: cal.c
+	> Author: jack
+	> Mail: jjy_8888@163.com 
+	> Brief: 
+	> Created Time: Fri 13 May 2016 07:16:04 AM EDT
+ ************************************************************************/
+#include<stdio.h>
+#include "token_parser.h"
+#include "cal.h"
+
+static double parser_term(TokenParser *thiz)
+{
+	
+}
+
+static double parser_expression(TokenParser *thiz)
+{
+		
+}
+
+double parser_line(char *line)
+{
+	
+}
+
---
+++ calculator/cal.h
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: cal.h
+	> Author: jack
+	> Mail: jjy_8888@163.com 
+	> Brief: 
+	> Created Time: Fri 13 May 2016 07:13:15 AM EDT
+ ************************************************************************/
+#ifndef CAL_H
+#define CAL_H
+
+double parser_line(char *line);
+
+#endif
+
